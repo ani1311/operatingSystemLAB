2567e058343044c90b29a1d2c1bafd6eddb395cb
ani1311 <aniket.alur13111997@gmail.com>
2019-03-09 22:09:47 +0530

    updating files

---
+++ shedulingAlgos.py
@@ -0,0 +1,79 @@
+
+program = [1,2,3,4,5]
+arrival_time = [0,0,0,0,0]
+burst_time = [9,4,5,7,3]
+priority = [5,3,1,2,4]
+
+def swap(i,j):
+    tp,ta,tb,tpri = program[i],arrival_time[i],burst_time[i],priority[i]
+    program[i],arrival_time[i],burst_time[i],priority[i] = program[j],arrival_time[j],burst_time[j],priority[j] 
+    program[j],arrival_time[j],burst_time[j],priority[j] = tp,ta,tb,tpri	
+
+def printProcesses():
+    n = len(program)
+    print("LIST OF PROCESSES WITH INFO") 
+    print("id          ",end="")
+    for i in range(n):
+        print("%5d"%program[i],end="")
+    
+    print("\narrival Time",end="")
+    for i in range(n):
+        print("%5d"%arrival_time[i],end="")
+
+    print("\nburst time  ",end="")
+    for i in range(n):
+        print("%5d"%burst_time[i],end="")
+    
+    print("\npriority    ",end="")
+    for i in range(n):
+        print("%5d"%priority[i],end="")
+    
+    print("\nEND")
+
+def sortByPriority():
+    for i in range(len(program)):
+        for j in range(len(program) - 1 - i):
+            if(priority[j] > priority[j + 1]):
+                swap(j,j+1)
+			
+def delProcess(i):
+    program.pop(i)
+    arrival_time.pop(i)
+    burst_time.pop(i)
+    priority.pop(i)
+
+def getNextProgram(t):
+    i = 0
+    while(arrival_time[i] > t):
+        i = i + 1
+    if(i == len(program)):
+        return -1
+    return i
+
+def priorptyNonPremptive():
+    t = 0
+    ans = []
+    while(len(program) != 0):
+        p = getNextProgram(t)
+        if(p == -1):
+            t = t + 1
+            continue
+        ans.append([program[p],t])
+        t = t + burst_time[p]
+        delProcess(p)
+        print()
+    
+    return ans
+
+def priorptyPremptive():
+    t = 0
+    ans = []
+
+
+
+
+printProcesses()	
+sortByPriority()
+printProcesses()
+print(priorptyNonPremptive())
+
